724f6e0614323d97fc43f53ea7b35c79f4e6b290
JED-6 <james.101.deslandes@gmail.com>
2025-02-13 13:04:06 +0000

    feat:added input form to webpage and moved other code files into packege folder

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,17 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["POST","GET"])
 def hello_world():
-    return 'Hello World!'
+    if request.method == "POST":
+        motions_content = request.form["content"]
+        return motions_content
+    else:
+        return render_template("index.html")
 
+if __name__=="__main__":
+    app.run(debug=True)
